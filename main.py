fcf8db1e40701ccfb1f269c44d7ddddbe50ea192
aayush <aayush29071996@gmail.com>
2017-07-08 15:50:01 +0530

    imported required python libraries

---
+++ main.py
@@ -0,0 +1,7 @@
+import turtle
+import matplotlib
+matplotlib.use('TkAgg')
+import numpy as np
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+from Tkinter import *
