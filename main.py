9861552798591da2d31fda10f80cd4c06b94b960
aayush <aayush29071996@gmail.com>
2017-07-08 15:52:29 +0530

    initial window popup

--- main.py
+++ main.py
@@ -5,3 +5,18 @@ import numpy as np
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 from Tkinter import *
+
+
+class mclass:
+
+    def __init__(self,  window):
+        self.window = window
+        self.button = Button (window, text="CLICK ME", command=self.plot,height = 50, width = 50)
+
+
+        self.button.pack()
+
+
+window= Tk()
+start= mclass (window)
+window.mainloop()
