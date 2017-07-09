648b1fbaf8f7971844ea4a98e70a449903b6cad5
aayush <aayush29071996@gmail.com>
2017-07-09 15:29:07 +0530

    output box optimized, bug fixes and warnings introduced

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ from Tkinter import *
 import tkMessageBox
 
 
+
 class mclass:
     def __init__(self, window):
         self.window = window
-        self.button = Button(window, text="CLICK ME TO START", command=self.plot, height=100, width=100)
+        self.button = Button(window, text="CLICK ME TO START", command=self.plot, height=100, width=500)
 
         self.button.pack()
 
@@ -22,20 +23,26 @@ class mclass:
 
         self.box = Entry(window, width=70, font=50)
 
+        self.box1 = Entry(window, width=70, font=50)
+
+
         self.box.pack()
+        Label(window, text="FINAL POSITION", font=30).pack()
+
+        self.box1.pack()
 
         self.button1.pack()
         self.button.destroy()
 
         # obstacles are set here
-        x = np.array([3, 4, 5, 6, 7, 8, 9, 2, 1, 10])
+        x = np.array([3, 6, 5, 6, 7, 8, 9, 2, 1, 10])
         y = np.array([3, 1, 6, 5, 2, 10, 3, 9, 10, 10])
 
         fig = Figure(figsize=(20, 20))
         a = fig.add_subplot(111)
         a.scatter(y, x, color='red')
 
-        a.set_title("OBSTACLE PLOT", fontsize=20)
+        a.set_title("OBSTACLE MAP", fontsize=20)
         a.set_ylabel("Y", fontsize=14)
         a.set_xlabel("X", fontsize=14)
 
@@ -44,10 +51,13 @@ class mclass:
         canvas.draw()
 
     def runTurtle(self):
+        self.box1.delete(0, 'end')
 
         let = []
         input1 = self.box.get()
-        turtle.setup(width=1400, height=800)
+        turtle.setup(width=1200, height=600)
+        turtle.bgcolor("lightgreen")
+
         my_start = (1, 1)
         turtle.setx(my_start[0])
         turtle.sety(my_start[1])
@@ -64,13 +74,16 @@ class mclass:
             if x == 'f' or x == 'F':
                 turtle.pendown()
                 turtle.forward(1)
-                if (turtle.position() == (3.00, 3.00) or turtle.position() == (1.00, 4.00) or turtle.position() == (
+                if(turtle.position()>=(10.00,10.00) or turtle.position()<=(0.00,0.00)):
+                    print "Turtle cant move out of bound"
+                if (turtle.position() == (3.00, 3.00) or turtle.position() == (1.00, 6.00) or turtle.position() == (
                 6.00, 5.00) or turtle.position() == (5.00, 6.00) or turtle.position() == (
                 2.00, 7.00) or turtle.position() == (10.00, 8.00) or turtle.position() == (
                 3.00, 9.00) or turtle.position() == (9.00, 2.00) or turtle.position() == (
                 10.00, 1.00) or turtle.position() == (10.00, 10.00)):
                     turtle.backward(1)
                     print "Obstacle on the way, Cant move forward!!, current position is ", turtle.position()
+                    tkMessageBox.showinfo("Warning!!", "OBSTACLE DETECTED!!")
 
                 turtle.penup()
 
@@ -87,6 +100,7 @@ class mclass:
 
             else:
                 print "Command could not be executed because ", x, " is an invalid command!!"
+                tkMessageBox.showinfo("Warning!!", "INVALID COMMAND FOUND!!")
 
         direction = ""
         if (turtle.heading() == 0.0):
@@ -105,9 +119,12 @@ class mclass:
             direction = "S"
 
         str1 =  turtle.position(),direction
+        print str1
 
-        tkMessageBox.showinfo("OUTPUT", str1)
-        print "The final position of the turtle is ", turtle.position(), " on XY plane"
+        print "The final position of the turtle is ", turtle.position(), " on XY plane in ", direction," direction"
+
+        self.box1.insert(0,str1)
+
 
 
 window = Tk()
