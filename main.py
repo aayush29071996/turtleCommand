b7258c49bb68f7ea5f1845cf4fbf990f3f9376c3
aayush <aayush29071996@gmail.com>
2017-07-09 15:32:55 +0530

    code identation optimization

--- main.py
+++ main.py
@@ -11,26 +11,20 @@ import tkMessageBox
 
 
 class mclass:
+
     def __init__(self, window):
         self.window = window
         self.button = Button(window, text="CLICK ME TO START", command=self.plot, height=100, width=500)
-
         self.button.pack()
 
     def plot(self):
         self.button1 = Button(window, text="RUN TURTLE", command=self.runTurtle, height=3, width=20)
         Label(window, text="INSERT COMMAND", font=30).pack()
-
         self.box = Entry(window, width=70, font=50)
-
         self.box1 = Entry(window, width=70, font=50)
-
-
         self.box.pack()
         Label(window, text="FINAL POSITION", font=30).pack()
-
         self.box1.pack()
-
         self.button1.pack()
         self.button.destroy()
 
@@ -41,7 +35,6 @@ class mclass:
         fig = Figure(figsize=(20, 20))
         a = fig.add_subplot(111)
         a.scatter(y, x, color='red')
-
         a.set_title("OBSTACLE MAP", fontsize=20)
         a.set_ylabel("Y", fontsize=14)
         a.set_xlabel("X", fontsize=14)
@@ -50,7 +43,10 @@ class mclass:
         canvas.get_tk_widget().pack()
         canvas.draw()
 
+
+
     def runTurtle(self):
+
         self.box1.delete(0, 'end')
 
         let = []
@@ -64,7 +60,6 @@ class mclass:
         turtle.setheading(90)
 
         i = 0
-
         for letter in input1:
             let.append(letter)
             i += 1
@@ -74,8 +69,10 @@ class mclass:
             if x == 'f' or x == 'F':
                 turtle.pendown()
                 turtle.forward(1)
+
                 if(turtle.position()>=(10.00,10.00) or turtle.position()<=(0.00,0.00)):
                     print "Turtle cant move out of bound"
+
                 if (turtle.position() == (3.00, 3.00) or turtle.position() == (1.00, 6.00) or turtle.position() == (
                 6.00, 5.00) or turtle.position() == (5.00, 6.00) or turtle.position() == (
                 2.00, 7.00) or turtle.position() == (10.00, 8.00) or turtle.position() == (
@@ -86,7 +83,6 @@ class mclass:
                     tkMessageBox.showinfo("Warning!!", "OBSTACLE DETECTED!!")
 
                 turtle.penup()
-
 
             elif x == 'l' or x == 'L':
                 turtle.pendown()
@@ -102,10 +98,12 @@ class mclass:
                 print "Command could not be executed because ", x, " is an invalid command!!"
                 tkMessageBox.showinfo("Warning!!", "INVALID COMMAND FOUND!!")
 
+
         direction = ""
         if (turtle.heading() == 0.0):
             print "Turtle is headed in EAST direction"
             direction = "E"
+
         elif (turtle.heading() == 90.0):
             print "Turtle is headed in NORTH direction"
             direction = "N"
@@ -118,11 +116,10 @@ class mclass:
             print "Turtle is headed in SOUTH direction"
             direction = "S"
 
+
         str1 =  turtle.position(),direction
         print str1
-
         print "The final position of the turtle is ", turtle.position(), " on XY plane in ", direction," direction"
-
         self.box1.insert(0,str1)
 
 
